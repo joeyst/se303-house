9b11604c2bebbef11814af14e7f5f1c2275edb08
joeyst <joeystenbeck@gmail.com>
2022-04-19 16:38:28 -0700

    Add logic for when line number equals three.

--- lib/house.rb
+++ lib/house.rb
@@ -2,6 +2,8 @@ class House
   def line(line_number)
     if (line_number == 2)
       return "This is the malt that lay in the house that Jack built.\n"
+    elsif (line_number == 3)
+      return "This is the rat that ate the malt that lay in the house that Jack built.\n"
     end
     "This is the house that Jack built.\n"
   end
