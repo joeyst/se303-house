09adb139838299ec2791aec63fa0bbc5394a3e07
joeyst <joeystenbeck@gmail.com>
2022-04-19 16:57:24 -0700

    Add logic for when line number equals ten.

--- lib/house.rb
+++ lib/house.rb
@@ -16,6 +16,8 @@ class House
       return "This is the man all tattered and torn that kissed the maiden all forlorn that milked the cow with the crumpled horn that tossed the dog that worried the cat that killed the rat that ate the malt that lay in the house that Jack built.\n"
     elsif (line_number == 9)
       return "This is the priest all shaven and shorn that married the man all tattered and torn that kissed the maiden all forlorn that milked the cow with the crumpled horn that tossed the dog that worried the cat that killed the rat that ate the malt that lay in the house that Jack built.\n"
+    elsif (line_number == 10)
+      return "This is the rooster that crowed in the morn that woke the priest all shaven and shorn that married the man all tattered and torn that kissed the maiden all forlorn that milked the cow with the crumpled horn that tossed the dog that worried the cat that killed the rat that ate the malt that lay in the house that Jack built.\n"
     end
     "This is the house that Jack built.\n"
   end
