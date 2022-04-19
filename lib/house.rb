d876950dc569e99d06835b6c4a9778c6c998d5eb
joeyst <joeystenbeck@gmail.com>
2022-04-19 16:54:17 -0700

    Add logic for when line number equals seven.

--- lib/house.rb
+++ lib/house.rb
@@ -10,6 +10,8 @@ class House
       return "This is the dog that worried the cat that killed the rat that ate the malt that lay in the house that Jack built.\n"
     elsif (line_number == 6)
       return "This is the cow with the crumpled horn that tossed the dog that worried the cat that killed the rat that ate the malt that lay in the house that Jack built.\n"
+    elsif (line_number == 7)
+      return "This is the maiden all forlorn that milked the cow with the crumpled horn that tossed the dog that worried the cat that killed the rat that ate the malt that lay in the house that Jack built.\n"
     end
     "This is the house that Jack built.\n"
   end
