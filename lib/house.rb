d1df0b9363ba34751c72a678645cbf52aafda95e
joeyst <joeystenbeck@gmail.com>
2022-04-19 16:37:23 -0700

    Add logic for when line number is two.

--- lib/house.rb
+++ lib/house.rb
@@ -1,5 +1,8 @@
 class House
   def line(line_number)
+    if (line_number == 2)
+      return "This is the malt that lay in the house that Jack built.\n"
+    end
     "This is the house that Jack built.\n"
   end
 end
