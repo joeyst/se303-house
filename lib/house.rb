218e1eb1cf2235bda520ad946cda1df3b325f8d9
joeyst <joeystenbeck@gmail.com>
2022-04-19 16:56:49 -0700

    Add logic for when line number equals nine.

--- lib/house.rb
+++ lib/house.rb
@@ -14,6 +14,8 @@ class House
       return "This is the maiden all forlorn that milked the cow with the crumpled horn that tossed the dog that worried the cat that killed the rat that ate the malt that lay in the house that Jack built.\n"
     elsif (line_number == 8)
       return "This is the man all tattered and torn that kissed the maiden all forlorn that milked the cow with the crumpled horn that tossed the dog that worried the cat that killed the rat that ate the malt that lay in the house that Jack built.\n"
+    elsif (line_number == 9)
+      return "This is the priest all shaven and shorn that married the man all tattered and torn that kissed the maiden all forlorn that milked the cow with the crumpled horn that tossed the dog that worried the cat that killed the rat that ate the malt that lay in the house that Jack built.\n"
     end
     "This is the house that Jack built.\n"
   end
