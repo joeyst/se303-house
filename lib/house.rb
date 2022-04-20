9c181447a19b4d5dd6c1f1b873edae22e0ae0ccb
joeyst <joeystenbeck@gmail.com>
2022-04-19 21:15:35 -0700

    Move case for when line number is one into case statement.

--- lib/house.rb
+++ lib/house.rb
@@ -1,5 +1,9 @@
 class House
   def line(line_number)
+    case line_number
+    when 1
+      return "This is the house that Jack built.\n"
+    end
     if (line_number == 2)
       return "This is the malt that lay in the house that Jack built.\n"
     elsif (line_number == 3)
@@ -23,7 +27,6 @@ class House
     elsif (line_number == 12)
       return "This is the horse and the hound and the horn that belonged to the farmer sowing his corn that kept the rooster that crowed in the morn that woke the priest all shaven and shorn that married the man all tattered and torn that kissed the maiden all forlorn that milked the cow with the crumpled horn that tossed the dog that worried the cat that killed the rat that ate the malt that lay in the house that Jack built.\n"
     end
-    "This is the house that Jack built.\n"
   end
 
   def recite
