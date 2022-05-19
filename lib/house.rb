a96cac96ed039c6c9ef0b22ec4515bfff1c8ad54
joeyst <joeystenbeck@gmail.com>
2022-05-19 15:24:59 -0700

    Remove inline dog method.

--- lib/house.rb
+++ lib/house.rb
@@ -9,7 +9,7 @@ class House
     "the dog that worried "]
 
   def line(line_number)
-      "This is #{appended_phrases(line_number)}#{dog(line_number)}#{cat(line_number)}#{rat(line_number)}#{malt(line_number)}the house that Jack built.\n"
+      "This is #{appended_phrases(line_number)}#{cat(line_number)}#{rat(line_number)}#{malt(line_number)}the house that Jack built.\n"
   end
 
   def recite
@@ -17,7 +17,7 @@ class House
   end
 
   def appended_phrases(line_number)
-    ((12-line_number)..(6)).map {|phrase_number| @@phrases[phrase_number]}.join("")
+    ((12-line_number)..(7)).map {|phrase_number| @@phrases[phrase_number]}.join("")
   end
 
   def dog(line_number)
