115cb64c4607ad1983b69c71633fb36d0768e0d8
joeyst <joeystenbeck@gmail.com>
2022-05-18 19:14:15 -0700

    Add tattered_and_torn phrase to phrases array.

--- lib/house.rb
+++ lib/house.rb
@@ -2,7 +2,8 @@ class House
   @@phrases = ["the horse and the hound and the horn that belonged to ", 
     "the farmer sowing his corn that kept ", 
     "the rooster that crowed in the morn that woke ",
-    "the priest all shaven and shorn that married "]
+    "the priest all shaven and shorn that married ",
+    "the man all tattered and torn that kissed "]
 
   def line(line_number)
       "This is #{appended_phrases(line_number)}#{tattered_and_torn(line_number)}#{maiden(line_number)}#{cow(line_number)}#{dog(line_number)}#{cat(line_number)}#{rat(line_number)}#{malt(line_number)}the house that Jack built.\n"
