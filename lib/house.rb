ee90d02de0f29991ee7161ae1cb110a1e628efeb
joeyst <joeystenbeck@gmail.com>
2022-06-03 17:15:20 -0700

    Add subject_phrases instance variable.

--- lib/house.rb
+++ lib/house.rb
@@ -4,16 +4,28 @@ class House
 
   def initialize
     @phrases = ["the horse and the hound and the horn that belonged to ", 
-    "the farmer sowing his corn that kept ", 
-    "the rooster that crowed in the morn that woke ",
-    "the priest all shaven and shorn that married ",
-    "the man all tattered and torn that kissed ",
-    "the maiden all forlorn that milked ",
-    "the cow with the crumpled horn that tossed ",
-    "the dog that worried ",
-    "the cat that killed ",
-    "the rat that ate ",
-    "the malt that lay in "]
+      "the farmer sowing his corn that kept ", 
+      "the rooster that crowed in the morn that woke ",
+      "the priest all shaven and shorn that married ",
+      "the man all tattered and torn that kissed ",
+      "the maiden all forlorn that milked ",
+      "the cow with the crumpled horn that tossed ",
+      "the dog that worried ",
+      "the cat that killed ",
+      "the rat that ate ",
+      "the malt that lay in "]
+
+    @subject_phrases = ["the horse and the hound and the horn",
+      "the farmer sowing his corn",
+      "the rooster that crowed in the morn",
+      "the priest all shaven and shorn",
+      "the man all tattered and torn",
+      "the maiden all forlorn",
+      "the cow with the crumpled horn",
+      "the dog",
+      "the cat",
+      "the rat",
+      "the malt"]
 
     # random seed used to consistently shuffle for testing
     @order = (0..phrases.length-1).to_a
