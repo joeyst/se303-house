91ba35826d0484e70a43446c8dbadd5c5b28fe50
joeyst <joeystenbeck@gmail.com>
2022-06-03 17:52:17 -0700

    Remove unused phrases instance variable.

--- lib/house.rb
+++ lib/house.rb
@@ -3,17 +3,6 @@ class House
   attr_reader :phrases, :subject_phrases, :verb_phrases
 
   def initialize
-    @phrases = ["the horse and the hound and the horn that belonged to ", 
-      "the farmer sowing his corn that kept ", 
-      "the rooster that crowed in the morn that woke ",
-      "the priest all shaven and shorn that married ",
-      "the man all tattered and torn that kissed ",
-      "the maiden all forlorn that milked ",
-      "the cow with the crumpled horn that tossed ",
-      "the dog that worried ",
-      "the cat that killed ",
-      "the rat that ate ",
-      "the malt that lay in "]
 
     @subject_phrases = ["the horse and the hound and the horn",
       "the farmer sowing his corn",
@@ -40,11 +29,11 @@ class House
       "lay in"]
 
     # random seed used to consistently shuffle for testing
-    @order = (0..phrases.length-1).to_a
+    @order = (0..subject_phrases.length-1).to_a
   end
 
   def one_phrase(phrase_number)
-    "#{@subject_phrases[phrase_number]} that #{verb_phrases[phrase_number]} "
+    "#{subject_phrases[phrase_number]} that #{verb_phrases[phrase_number]} "
   end
 
   def line(line_number)
@@ -61,12 +50,12 @@ class House
   end
 
   def randomize
-    self.order = (0..phrases.length-1).to_a.shuffle(random: Random.new(1))
+    self.order = (0..subject_phrases.length-1).to_a.shuffle(random: Random.new(1))
     self
   end
 
   def unrandomize
-    self.order = (0..phrases.length-1).to_a
+    self.order = (0..subject_phrases.length-1).to_a
     self
   end
 end
